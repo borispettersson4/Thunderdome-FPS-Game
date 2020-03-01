caa37b1e86003cd42d7a8a020e1c773adbe5c31a
Boris Pettersson <borisruiz4@users.noreply.github.com>
2020-03-01 21:02:34 +0100

    post

---
+++ Assets/Extra Scripts/LoadSceneImmediately.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneImmediately : MonoBehaviour
+{
+    public string sceneName;
+
+    void Start()
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+}
